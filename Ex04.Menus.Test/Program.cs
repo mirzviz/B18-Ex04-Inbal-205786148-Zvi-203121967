f248c893ac8887aab14a9d2c1002fe58e0c7120d
Zvi Lychee <mirzviz@gmail.com>
2018-05-13 13:30:47 +0300

    initianl commit :)

---
+++ Ex04.Menus.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04.Menus.Test
+{
+    public class Program
+    {
+        public delegate void MyDel();
+        public static void Main()
+        {
+            MyDel del = new MyDel(func1);
+            del += func2;
+            del.Invoke();
+        }
+
+        public static void func1()
+        {
+            Console.WriteLine("func1");
+        }
+
+        public static void func2()
+        {
+            Console.WriteLine("func2");
+        }
+    }
+}
